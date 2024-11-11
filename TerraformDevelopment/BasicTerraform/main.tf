21394597f584e5aa71eeb9c6f3dbc3bee899a168
AashishSK333 <sk9ashish@gmail.com>
2024-11-11 18:25:24 +0800

    basic terraform scripts

---
+++ TerraformDevelopment/BasicTerraform/main.tf
@@ -0,0 +1,15 @@
+# Configuring the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
