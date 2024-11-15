0d098da861f92740d7b5c12d9d1afaaa7caee1d7
AashishSK333 <sk9ashish@gmail.com>
2024-11-15 15:29:57 +0800

    Python Datatypes

--- PythonApplication/TestApplications/DemoHelloWorld/HelloWorld.py
+++ PythonApplication/TestApplications/DemoHelloWorld/HelloWorld.py
@@ -1,1 +1,15 @@
-print("Hello World! Mr Ashish")
+string1 = '''hello'''
+dict1 = {"my-key":123, "random":456}
+dict2 = {"season":"Summmer"}
+dict1.update(dict2)
+
+set1 = {"a", "b"}
+set1.add(5)
+
+list1 = [1,2,3,4]
+list1.append(5)
+
+print("Sets value is: ".format(), set1)
+print("List value is: ".format(),list1)
+print("Value of dictonary is: ".format(), dict1)
+print(string1)
